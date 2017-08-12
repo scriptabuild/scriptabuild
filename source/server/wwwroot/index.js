08aed8b9e34ccbb5c3dff28f2818b67a8950dd0a
Arjan Einbu <arjan@einbu.no>
2017-08-12 14:47:25 +0200

    List projects on index.html

--- source/server/wwwroot/index.js
+++ source/server/wwwroot/index.js
@@ -1,22 +1,28 @@
 console.log("Welcome to Scriptabuild server");
 
+let throwHttpError = (response) => {
+	throw new Error(`Fetch(...) returned an error: ${response.status} ${response.statusText}`);
+}
+let getJsonOrFailOnHttpError = (response) => response.ok? response.json(): throwHttpError(response)
+
 console.log("---fetching all projects...")
 fetch("http://localhost:8080/api/project-list")
-	.then(response => response.json())
-	.then(data => {
-		console.log("fetching all projects succeeded:", data);
-	})
-	.catch(err => {
-		console.log("fetching all projects failed:", err);
-	});
+    .then(getJsonOrFailOnHttpError)
+    .then(projects => {
+        console.log("fetching all projects succeeded:", projects);
+        ko.applyBindings({projects})
+    })
+    .catch(err => {
+        console.log("fetching all projects failed:", err);
+    });
+
 
 console.log("---fetching a project...")
 fetch("http://localhost:8080/api/project-detail/Script-a-build")
-	.then(response => response.json())
-	.then(data => {
-		console.log("fetching a project succeeded:", data);
-	})
-	.catch(err => {
-		console.log("fetching a project failed:", err);
-	});
-
+    .then(getJsonOrFailOnHttpError)
+    .then(data => {
+        console.log("fetching a project succeeded:", data);
+    })
+    .catch(err => {
+        console.log("fetching a project failed:", err);
+    });
