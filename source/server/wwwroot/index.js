e6214a10812c5cee1d329df2d2cdfb77881d9ea3
Arjan Einbu <arjan@einbu.no>
2017-08-13 20:25:37 +0200

    Added build button++

--- source/server/wwwroot/index.js
+++ source/server/wwwroot/index.js
@@ -1,28 +1,47 @@
 console.log("Welcome to Scriptabuild server");
 
 let throwHttpError = (response) => {
-	throw new Error(`Fetch(...) returned an error: ${response.status} ${response.statusText}`);
+    throw new Error(`Fetch(...) returned an error: ${response.status} ${response.statusText}`);
 }
-let getJsonOrFailOnHttpError = (response) => response.ok? response.json(): throwHttpError(response)
+let getJsonOrFailOnHttpError = (response) => response.ok ? response.json() : throwHttpError(response)
 
-console.log("---fetching all projects...")
-fetch("http://localhost:8080/api/project-list")
-    .then(getJsonOrFailOnHttpError)
-    .then(projects => {
-        console.log("fetching all projects succeeded:", projects);
-        ko.applyBindings({projects})
-    })
-    .catch(err => {
-        console.log("fetching all projects failed:", err);
-    });
+let loadProject = (project) => {
+    let status = ko.observable("[loading]");
+    project.status = status;
+    fetch(`/api/project-summary/${project.id}`)
+        .then(getJsonOrFailOnHttpError)
+        .then(res => void(status(res.current.buildStatus)))
+        .catch(() => void(status("never built")));
+}
+
+let loadProjects = (vm) => {
+    fetch("/api/project-list")
+        .then(getJsonOrFailOnHttpError)
+        .then(projects => {
+            projects.forEach(loadProject);
+            vm.projects.push(...projects);
+        })
+        .catch(err => void(console.log("fetching all projects failed:", err)));
+
+}
+
+let buildProject = (project) => {
+	fetch(`/api/project-build/${project.id}`, {method: "post"})
+		.then(getJsonOrFailOnHttpError)
+		.then(() => void(project.status("requesting build")))
+		.catch(() => void(project.status("request failed")));
+}
+
+//------------
 
 
-console.log("---fetching a project...")
-fetch("http://localhost:8080/api/project-detail/Script-a-build")
-    .then(getJsonOrFailOnHttpError)
-    .then(data => {
-        console.log("fetching a project succeeded:", data);
-    })
-    .catch(err => {
-        console.log("fetching a project failed:", err);
-    });
+
+let vm = {
+	projects: ko.observableArray([]),
+	buildProject
+}
+
+ko.applyBindings(vm);
+loadProjects(vm);
+
+
